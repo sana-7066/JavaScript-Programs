c104287b97c07d712ddc13d24e9ae80b7563a6c6
sana u shaikh <hkiahs66@gmail.com>
2022-07-20 10:51:06 +0530

    joinarray added

---
+++ joinArr19.js
@@ -0,0 +1,11 @@
+/**
+ * joining array / converting array into string using + & , operator 
+ * you can convert array to string using toString() function
+*/
+
+function joinArr(arr,oper)
+{
+    return arr.join(oper)
+}
+
+console.log(joinArr(["red","green","white","black"],","))
