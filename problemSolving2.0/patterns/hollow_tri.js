c79b63e0cb20c82bf9603d05526609dab6c2582c
sana u shaikh <hkiahs66@gmail.com>
2023-01-04 11:42:26 +0530

    patterns added

--- problemSolving2.0/patterns/hollow_tri.js
+++ problemSolving2.0/patterns/hollow_tri.js
@@ -8,17 +8,43 @@ hollow triangle like this
 //n-i spaces , 2*i-1 stars for all fill
 */
  
-let n=4,str="";
-for(i=1;i<=n;i++)
+let n=9,str="";
+for(i=1;i<=n;i++)// each row
 {
-  for(j=1;j<=n-i;j++)
+  for(j=1;j<=n-i;j++)// front spaces for creating triangle
   {
     str+=" "
   }
-  for(k=1;k<=2*i-1;i++)
+  for(k=1;k<=2*i-1;k++)// printing stars
   {
+    if(i===1 || i===n)// first and last row contain only stars not spaces 
+    {
     str+="*"
+    }
+    else{
+      if(k===1|| k===2*i-1) // filling edges with stars
+      {
+        str+="*"
+      }
+      else                // filling spaces in between 
+      {
+        str+=" "
+      }
+    }
   }
   str+="\n"
 }
 console.log(str)
+
+// o/p:
+/*
+        *
+       * *
+      *   *
+     *     *
+    *       *
+   *         *
+  *           *
+ *             *
+*****************
+*/
